9b215c09ccd34c49f46d8b40e42205a4b1d972bd
sajid karim <80595807+sajid1997@users.noreply.github.com>
2026-01-21 17:16:06 +0500

    Create salary is higher than their department average salary .sql
    👉 Write a SQL query to return employees whose salary is higher than their department average salary.
    
    Return:
    EmployeeName
    Department
    Salary
    AvgDepartmentSalary
    Write the query.

---
+++ salary is higher than their department average salary .sql
@@ -0,0 +1,28 @@
+ /*✅ Question 3
+
+👉 Write a SQL query to return employees whose salary is higher than their department average salary.
+
+Return:
+EmployeeName
+Department
+Salary
+AvgDepartmentSalary
+Write the query.*/
+
+With CTE 
+AS
+(
+ Select EmployeeName,Department, Salary
+ From mulazim 
+),
+ AVG_SALARY
+ as
+(
+ Select Department, AVg(cast(Salary as int)) as AvgDepartmentSalary 
+ from mulazim 
+ Group by Department
+)
+Select C.EmployeeName, C.Department, C.Salary, A.AvgDepartmentSalary
+from CTE C 
+Inner Join AVG_SALARY A On A.Department = C.Department
+Where Salary > AvgDepartmentSalary
