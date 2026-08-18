8f8ef4f090cbe70f369638784a9d4b7ed6232a23
sajid karim <80595807+sajid1997@users.noreply.github.com>
2026-08-18 12:51:40 +0500

    Update Filtering Data.sql

--- Filtering Data.sql
+++ Filtering Data.sql
@@ -29,7 +29,7 @@ D.DepartmentName in ('IT', 'HR', 'Finance')
 --🔥 Excellent! 10/10 — Correct. Your query is exactly right:
 
 /*Question 3 — Advanced 🔴
-
+  
 Write a query to display the EmployeeName, Age, and DepartmentName of employees
 who are NOT from the IT or HR departments and whose age is NOT between 25 and 30.*/
 
@@ -39,3 +39,23 @@ where D.DepartmentName not in ('It', 'HR') and
 Age Not between 23 and 30;
 
 --100% correct. ✅
+
+
+/*Question 4 — Advanced 🔴
+
+Write a query to display the EmployeeName, Age, and DepartmentName of employees whose name starts with the letter
+A OR ends with the letter n, and whose age is greater than 25.*/
+
+
+ Select EmployeeName, Age, D.DepartmentName From Employees as E
+ inner join Department AS D On D.DepartmentID = E.DepartmentName 
+ where (E.EmployeeName like 'A%' OR E.EmployeeName like '%N') AND Age > 25;
+ --10/10 🔥
+
+ /*Question 5 — Advanced 🔴
+
+Write a query to display the EmployeeName, Age, and DepartmentName of employees whose name contains the
+letter a anywhere in the name, but does not start with A.*/
+
+select EmployeeName, Age, DepartmentName from employees 
+Where EmployeeName Like '%A%' AND EmployeeName not like 'A%'
